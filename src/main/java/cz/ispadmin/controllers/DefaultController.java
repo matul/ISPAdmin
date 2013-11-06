7ddbd0e715af01e7b1bb0486305801e03fbcaf37
beda <beda@jsemklasik.cz>
2013-11-06 20:15:35 +0100

    Oprava spuštění aplikace

--- src/main/java/cz/ispadmin/controllers/DefaultController.java
+++ src/main/java/cz/ispadmin/controllers/DefaultController.java
@@ -1,29 +1,18 @@
 package cz.ispadmin.controllers;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Roman
  */
 @Controller
-@RequestMapping("/ispadmin")
+@RequestMapping("/")
 public class DefaultController {
   
-  @RequestMapping
+  @RequestMapping("/")
   public String StartPage() {
-    return "AHOJ";
+    return "index";
   }  
-
-  @RequestMapping(value = "/kotel", method = RequestMethod.GET)
-  public ModelAndView handleResults(HttpServletRequest request, HttpServletResponse response) {
-    ModelAndView modelAndView = new ModelAndView("index");
-    return modelAndView;
-  }
   
 }
