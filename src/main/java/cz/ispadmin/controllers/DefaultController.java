1d9dc4f01001b2bb23e7c690a3c40dcfedde0653
Roman <Roman@Roman-NB>
2013-11-09 17:49:56 +0100

    Hrajeme si s Javou

--- src/main/java/cz/ispadmin/controllers/DefaultController.java
+++ src/main/java/cz/ispadmin/controllers/DefaultController.java
@@ -1,18 +1,46 @@
 package cz.ispadmin.controllers;
+import cz.ispadmin.models.*;
 
 import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Roman
  */
+
 @Controller
 @RequestMapping("/")
 public class DefaultController {
   
+  private ModelAndView modelAndView;
+  private KotelModel kotelModel;
+
+  public DefaultController() {
+    this.modelAndView = new ModelAndView();
+  }
+
+  @Autowired
+  public void setKotelModel(KotelModel kotelModel) {
+    this.kotelModel = kotelModel;
+  }
+  
   @RequestMapping("/")
-  public String StartPage() {
-    return "index";
-  }  
+  public ModelAndView StartPage(HttpServletRequest request, HttpServletResponse response) {
+    modelAndView.setViewName("index");
+    return modelAndView;
+  }
+  
+  @RequestMapping("/kotel/{ahoj}")
+  public ModelAndView dejKotel(HttpServletRequest request, HttpServletResponse response) {
+    modelAndView.addObject("kotelMessage", kotelModel.getKotel());
+    modelAndView.setViewName("kotel");
+    return modelAndView;
+  } 
   
 }
---
+++ src/main/java/cz/ispadmin/models/KotelModel.java
@@ -0,0 +1,15 @@
+package cz.ispadmin.models;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Roman
+ */
+@Service
+public class KotelModel {
+  
+  public String getKotel() {
+    return "Dostal jsi kotel";
+  }
+  
+}
